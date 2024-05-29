5a299ccd621bc6036cb2f137c8f37dc371512dec
Rajesh Choudhary <rajuvert18@gmail.com>
2024-05-29 12:33:53 +0530

    overflow fixed

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -14,7 +14,7 @@ const intro = `Rajesh Choudhary`;
 const Hero: FC = (props: Props) => {
   return (
     <section className="px-8 relative    h-[calc(100svh)] grid sm:grid-cols-2">
-      <div className="absolute top-0 z-[-2] hidden dark:block h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+      <div className="absolute top-0 z-[-2] hidden dark:block h-full w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
       <div className="absolute hidden sm:block inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
       </div>
